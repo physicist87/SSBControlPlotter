d3a603d5f74d489a14a68155070b7a8dfbefceb7
Seungkyu Ha <physicist87@gmail.com>
2025-07-15 16:59:41 +0900

    updated run.sh : use StudyName

--- run.sh
+++ run.sh
@@ -2,21 +2,34 @@
 
 # Define the executable and input files
 EXEC="./StackAndOverlayHistograms"
+STUDYNAME="NanoAOD_v3" 
+#STUDYNAME="NanoAOD_v2" 
 #INPUT_LIST="input/Testv4p1/UL2016PostVFP/MuMu/Testv4p1_UL2016PostVFP_MuMu.list"
 #INPUT_LIST="input/NanoAOD_v1/UL2018/MuMu/NanoAOD_v1_UL2018_MuMu.list"
 INPUT_LIST="input/NanoAOD_v1p2/UL2018/MuMu/NanoAOD_v1p2_UL2018_MuMu.list"
 INPUT_LIST="input/NanoAOD_v1p2/UL2016PreVFP/MuMu/NanoAOD_v1_UL2016PreVFP_MuMu.list"
 INPUT_LIST="input/NanoAOD_v1/UL2016PreVFP/MuMu/NanoAOD_v1_UL2016PreVFP_MuMu.list"
+INPUT_LIST="input/NanoAOD_v2/UL2017/MuMu/NanoAOD_v2_UL2017_MuMu.list"
+INPUT_LIST="input/NanoAOD_v3/UL2018/MuMu/NanoAOD_v3_UL2018_MuMu.list"
+INPUT_LIST="input/${STUDYNAME}/UL2018/MuMu/${STUDYNAME}_UL2018_MuMu.list"
+
 COLOR_CONFIG="ColorConfig.txt"
 SCALE_CONFIG="ScaleConfig.txt"
 HIST_CONFIG="HistConfig.txt"
+
 OUTPUT_DIR="NanoAOD_v1/UL2018/MuMu"
 OUTPUT_DIR="NanoAOD_v1p2/UL2018/MuMu"
 OUTPUT_DIR="NanoAOD_v1/UL2016PreVFP/MuMu"
+OUTPUT_DIR="ExtendABCD_v1/UL2018/MuMu"
+OUTPUT_DIR="NanoAOD_v2/UL2018/MuMu"
+OUTPUT_DIR="NanoAOD_v2/UL2017/MuMu"
+OUTPUT_DIR="NanoAOD_v3/UL2018/MuMu"
+OUTPUT_DIR="${STUDYNAME}/UL2018/MuMu"
 LUMI_TEXT="UL 2018 13 TeV"        # Add luminosity text
 LUMI_TEXT="UL 2016APV 13 TeV"     # Add luminosity text
 LUMI_TEXT="59.83 fb^{-1} (13 TeV)" # Add luminosity text 2018
-LUMI_TEXT="19.65 fb^{-1} (13 TeV)" # Add luminosity text 2016 PreVFP
+#LUMI_TEXT="47.48 fb^{-1} (13 TeV)" # Add luminosity text 2017
+#LUMI_TEXT="19.65 fb^{-1} (13 TeV)" # Add luminosity text 2016 PreVFP
 
 # Run the executable with the provided arguments
 echo $EXEC $INPUT_LIST $COLOR_CONFIG $SCALE_CONFIG $OUTPUT_DIR \"$LUMI_TEXT\"
